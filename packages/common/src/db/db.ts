6201e648182d2ccf666f89f5476f525e44bfc24b
Peter Kudela <peter.kudela@cargo-partner.com>
2025-10-12 20:53:18 +0200

    db whole schema

--- packages/common/src/db/db.ts
+++ packages/common/src/db/db.ts
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import "dotenv/config";
-import { titlesMediaTable, titlesTable } from "./schema.js";
+import { schema } from "./schema.js";
 
 export const db = drizzle({
   client: new Pool({
     connectionString: process.env.POSTGRES_URL,
   }),
-  schema: { titlesTable, titlesMediaTable },
+  schema: { schema },
 });
