dc33d3bce20e17c3d16cafa1f4178bf2713d7b90
Peter Kudela <peter.kudela@cargo-partner.com>
2025-10-28 12:01:19 +0100

    fix: update TitlesPoster to use ImageOff icon when no image is available

--- apps/web/src/ui/titles/TitlesPoster.tsx
+++ apps/web/src/ui/titles/TitlesPoster.tsx
@@ -1,24 +1,15 @@
 import { TitleListDto } from "@repo/common";
 import Image from "next/image";
+import { ImageOff } from "lucide-react";
 
 export default function TitlesPoster({ title }: { title: TitleListDto }) {
   if (!title.images[0]) {
-    return <svg width={120} height={180} className="w-auto h-[180px] bg-gray-200">
-      <rect width="120" height="180" fill="#e5e7eb"/>
-      <text
-        x="60"
-        y="90"
-        textAnchor="middle"
-        dominantBaseline="middle"
-        fontSize="16"
-        fill="#6b7280"
-      >
-        not available
-      </text>
-    </svg>
+    return <div className="relative w-[120px] h-[180px] py-2 px-2 flex items-center justify-center">
+      <ImageOff size={48} className="text-gray-400"/>
+    </div>
   }
   return (
-    <div className={"relative w-[120px] h-[180px]"}>
+    <div className={"relative w-[120px] h-[180px] py-2 px-2"}>
       <Image
         src={title.images[0].url} width={0} height={0} sizes="100vh"
         style={{ height: '100%', width: 'auto' }} alt={""}/>
