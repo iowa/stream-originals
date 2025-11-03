00bfc3c6c0b8220cc990d56cfb7408195f144b39
Peter Kudela <peter.kudela@cargo-partner.com>
2025-11-03 15:25:55 +0100

    TitleAvatar work

--- apps/web/src/lib/streamer/StreamerLogo.tsx
+++ apps/web/src/lib/streamer/StreamerLogo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 export default function StreamerLogo({ streamer }: { streamer: Streamer }) {
   return (
     <div className="py-1 px-1">
-      <div style={{ width: 98.67, height: 26.67, position: 'relative' }}>
+      <div style={{ width: 98, height: 26, position: 'relative' }}>
         <Image
           src={StreamerUtils.getLogoSVG(streamer)}
           alt={`${streamer}`}
--- apps/web/src/ui/credit/TitleAvatar.tsx
+++ apps/web/src/ui/credit/TitleAvatar.tsx
@@ -1,16 +1,19 @@
 import { CreditTitleDto } from "@repo/common";
+import TitlesPoster from "@/ui/titles/TitlesPoster";
+import { TitleUtils } from "@/lib/title/TitleUtils";
 import Link from "next/link";
 import { Paths } from "@/lib/Paths";
-import TitlesPoster from "@/ui/titles/TitlesPoster";
 
 export default function TitleAvatar({ title }: { title: CreditTitleDto }) {
   return (
-    <Link href={Paths.title(title.id)} passHref
-          className="link link-hover overflow-hidden group">
-      <div className="flex items-center gap-4">
-        <TitlesPoster title={title} images={title.images} width={144} height={216}/>
-        <div>
-          <div className="font-bold">{title.name}</div>
+    <Link href={Paths.title(title.id)}>
+      <div className="card card-side bg-base-100 shadow-sm">
+        <div className="flex items-center">
+          <TitlesPoster title={title} images={title.images} width={144} height={216}/>
+        </div>
+        <div className="card-body">
+          <h2 className="card-title text-sm">{title.name}</h2>
+          <span>{TitleUtils.getYearRange(title.premiere)}</span>
         </div>
       </div>
     </Link>
