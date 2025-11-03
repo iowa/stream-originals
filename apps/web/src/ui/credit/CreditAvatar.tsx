2a01ab659f096107a0c45e1dbf51e71e37ab8ceb
Peter Kudela <peter.kudela@cargo-partner.com>
2025-11-03 09:42:23 +0100

    CreditAvatar link

--- apps/web/src/ui/credit/CreditAvatar.tsx
+++ apps/web/src/ui/credit/CreditAvatar.tsx
@@ -1,29 +1,33 @@
 import { TitleCreditWithCredit } from "@repo/common";
 import Image from "next/image"
 import { ImageOff } from "lucide-react";
+import Link from "next/link";
+import { Paths } from "@/lib/Paths";
 
 export default function CreditAvatar({ credit }: { credit: TitleCreditWithCredit }) {
   return (
-    <div className="flex items-center gap-4">
-      {credit.credit.primaryImageUrl ?
-        <div className="avatar">
-          <div className="w-36 rounded">
-            <Image
-              src={credit.credit.primaryImageUrl || ''}
-              alt={`${credit.credit.name}_avatar`}
-              fill={true}
-            />
+    <Link href={Paths.credit(credit.creditId)} passHref className="link link-hover overflow-hidden group">
+      <div className="flex items-center gap-4">
+        {credit.credit.primaryImageUrl ?
+          <div className="avatar">
+            <div className="w-36 rounded">
+              <Image
+                src={credit.credit.primaryImageUrl || ''}
+                alt={`${credit.credit.name}_avatar`}
+                fill={true}
+                className="group-hover:brightness-90"
+              />
+            </div>
           </div>
+          :
+          <div className="flex  justify-center items-center">
+            <ImageOff size={48} className="h-36 w-36 text-gray-400 group-hover:brightness-90"/>
+          </div>
+        }
+        <div>
+          <div className="font-bold">{credit.credit.name}</div>
         </div>
-        :
-        <div className="flex  justify-center items-center">
-          <ImageOff size={48} className="h-36 w-36 text-gray-400"/>
-        </div>
-      }
-      <div>
-        <div className="font-bold">{credit.credit.name}</div>
       </div>
-    </div>
-
+    </Link>
   );
 };
