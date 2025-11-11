3c2141ec46fa840e715e15d9d3555754cf665503
Peter Kudela <peter.kudela@cargo-partner.com>
2025-11-11 12:10:19 +0100

    vitest

--- packages/common/src/titles/__test__/TitlesListRepository.test.ts
+++ packages/common/src/titles/__test__/TitlesListRepository.test.ts
@@ -1,21 +1,21 @@
-import { afterEach, beforeEach, describe, expect, it } from "vitest";
-import { getDbMock } from "../../db/dbMock.js";
-import { DbDrizzle } from "../../db/dbDrizzle.js";
-import { PGlite } from "@electric-sql/pglite";
-import { TitlesListRepository } from "../TitlesListRepository.js";
-import { TestEntities } from "../../utils/testing/TestEntities.js";
-import { TitlesRepository } from "../TitlesRepository.js";
-import { TestData } from "../../utils/testing/TestData.js";
+import {afterEach, beforeEach, describe, expect, it} from 'vitest';
+import {getDbMock} from '../../db/dbMock.js';
+import {DbDrizzle} from '../../db/dbDrizzle.js';
+import {PGlite} from '@electric-sql/pglite';
+import {TitlesListRepository} from '../TitlesListRepository.js';
+import {TestEntities} from '../../utils/testing/TestEntities.js';
+import {TitlesRepository} from '../TitlesRepository.js';
+import {TestData} from '../../utils/testing/TestData.js';
 
-describe("TitlesListRepository", async () => {
-  let pgClient: PGlite
-  let titlesRepository: TitlesRepository
-  let testEntities: TestEntities
+describe('TitlesListRepository', async () => {
+  let pgClient: PGlite;
+  let titlesRepository: TitlesRepository;
+  let testEntities: TestEntities;
   let cut: TitlesListRepository;
 
   beforeEach(async () => {
-    let { client, db } = await getDbMock();
-    pgClient = client
+    let {client, db} = await getDbMock();
+    pgClient = client;
     titlesRepository = new TitlesRepository(db as unknown as DbDrizzle);
     cut = new TitlesListRepository(db as unknown as DbDrizzle);
     testEntities = new TestEntities(titlesRepository, undefined, undefined);
@@ -25,51 +25,36 @@ describe("TitlesListRepository", async () => {
     await pgClient.close();
   });
 
-  it("pageSize", async () => {
-    await prepareData()
+  it('pageSize', async () => {
+    await prepareData();
 
     const result1 = await cut.getTitles('netflix', 1, 1);
 
-    expect(result1.map(value => value.name)).toEqual(
-      [
-        "Stranger Things",
-      ]
-    )
+    expect(result1.map(value => value.name)).toEqual(['House of Cards']);
     const result2 = await cut.getTitles('netflix', 1, 2);
 
-    expect(result2.map(value => value.name)).toEqual(
-      [
-        "Stranger Things",
-        "House of Cards"
-      ]
-    )
-  })
+    expect(result2.map(value => value.name)).toEqual([
+      'House of Cards',
+      'Midnight Mass',
+    ]);
+  });
 
-  it("page", async () => {
-    await prepareData()
+  it('page', async () => {
+    await prepareData();
 
     const result1 = await cut.getTitles('netflix', 1, 2);
 
-    expect(result1.map(value => value.name)).toEqual(
-      [
-        "Stranger Things",
-        "House of Cards"
-      ]
-    )
+    expect(result1.map(value => value.name)).toEqual([
+      'House of Cards', 'Midnight Mass'
+    ]);
     const result2 = await cut.getTitles('netflix', 2, 2);
 
-    expect(result2.map(value => value.name)).toEqual(
-      [
-        "The Watcher"
-      ]
-    )
-  })
-
+    expect(result2.map(value => value.name)).toEqual(['The Watcher']);
+  });
 
   async function prepareData() {
-    await testEntities.insertTitle(TestData.TestTitle_HouseOfCards)
-    await testEntities.insertTitle(TestData.TestTitle_TheWatcher)
-    await testEntities.insertTitle(TestData.TestTitle_MidnightMass)
+    await testEntities.insertTitle(TestData.TestTitle_HouseOfCards);
+    await testEntities.insertTitle(TestData.TestTitle_MidnightMass);
+    await testEntities.insertTitle(TestData.TestTitle_TheWatcher);
   }
-
 });
